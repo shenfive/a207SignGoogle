14b1b961bcfc7cf8c93dd9db17fccdebbafc5fed
Danny Shen <shenfive@gmail.com>
2020-08-29 14:32:59 +0800

    google Sign Out

--- a207SignGoogle/ViewController.swift
+++ a207SignGoogle/ViewController.swift
@@ -8,17 +8,56 @@
 
 import UIKit
 import GoogleSignIn
+import Firebase
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var signOutBut: UIButton!
     
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance().signIn()
+        
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let user = user{
+                self.setSignedUI()
+            }else{
+                self.setSignOutUI()
+            }
+        }
     }
-
+    
+    func setSignedUI(){
+        signInButton.isEnabled = false
+        signOutBut.isEnabled = true
+        if let username = Auth.auth().currentUser?.displayName{
+            statusLabel.text = "歡迎：" + username
+        }
+    }
+    
+    func setSignOutUI(){
+        signInButton.isEnabled = true
+        signOutBut.isEnabled = false
+        statusLabel.text = "請登入"
+        
+    }
+    
+    
+    
+    @IBAction func signOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch  {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+    }
+    
 
 }
 
